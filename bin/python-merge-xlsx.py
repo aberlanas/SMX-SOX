918e5a73df952918555e9c6190a323ee0fdcef0f
Angel Berlanas Vicente <angel.berlanas@gmail.com>
2021-10-31 08:26:58 +0100

    Added binary

---
+++ bin/python-merge-xlsx.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# This Script is licensed under GPLv3 or higher.
+# Author : Angel Berlanas Vicente
+#          <aberlanas@disroot.org>
+
+# Import section
+import sys
+
+
+
+
+
+if ( __name__ == "__main__" ):
+    print(" * XLSX Merger ")
+    sys.exit(0)
